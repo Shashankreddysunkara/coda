661045816922735ed2bc8844873d0ba791c07d43
Shashank Reddy Sunkara <inusasunny@gmail.com>
2021-02-17 19:03:13 +0530

    Update image-build.Unix.sh

--- images/master/bin/image-build.Unix.sh
+++ images/master/bin/image-build.Unix.sh
@@ -4,7 +4,10 @@ exit_code=0;
 
 mkdir -p keys;
 
-cp -r 'coda/settings/keys/jenkins.*' ./keys &&\
+cp 'coda/settings/keys/jenkins.application.id_rsa' ./keys &&\
+    cp 'coda/settings/keys/jenkins.application.id_rsa.pub' ./keys &&\
+    cp 'coda/settings/keys/jenkins.config.id_rsa' ./keys &&\
+    cp 'coda/settings/keys/jenkins.config.id_rsa.pub' ./keys &&\
     docker build --no-cache -t jenkins-master .;
 if [[ "${?}" != 0 ]]; then
     exit_code=1;
