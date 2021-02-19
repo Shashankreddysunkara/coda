1866de0bbfe4c8e364a5a50275c31f547328cb19
Shashank Reddy Sunkara <inusasunny@gmail.com>
2021-02-20 03:47:40 +0530

    update with volumes

--- images/master/bin/container-run.Unix.sh
+++ images/master/bin/container-run.Unix.sh
@@ -1,9 +1,30 @@
 #!/usr/bin/env bash
+#
+#exit_code=0;
+#
+#docker run \
+#    -d \
+#    --name jenkins-master \
+#    -p 8080:8080 \
+#    -p 50000:50000 \
+#    -v $(which docker):/usr/bin/docker \
+#    -v /var/run/docker.sock:/var/run/docker.sock \
+#    -v /root/workspace \
+#    jenkins-master &&\
+#
+#if [[ "${?}" != 0 ]]; then
+#    exit_code=1;
+#fi;
+#
+#exit $((exit_code));
+#
+#sudo docker logs -f jenkins-master;
 
 exit_code=0;
 
-chmod 777 /usr/local/bin/init.sh;
-
+docker volume create jenkins-data
+docker volume create jenkins-log
+docker volume ls
 docker run \
     -d \
     --name jenkins-master \
@@ -11,13 +32,14 @@ docker run \
     -p 50000:50000 \
     -v $(which docker):/usr/bin/docker \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /root/workspace \
+    -v /root:/root/workspace \
+    --mount source=jenkins-log,target=/var/log/jenkins \
+    --mount source=jenkins-data,target=/var/jenkins_home \
     jenkins-master &&\
+    sudo docker logs -f jenkins-master;
 
 if [[ "${?}" != 0 ]]; then
     exit_code=1;
 fi;
 
 exit $((exit_code));
-
-sudo docker logs -f jenkins-master;
