60749d0c1151648a078370f9461d4907ce7747df
Shashank Reddy Sunkara <inusasunny@gmail.com>
2021-02-26 18:51:39 +0530

    Update image-build.Unix.sh

--- images/master/bin/image-build.Unix.sh
+++ images/master/bin/image-build.Unix.sh
@@ -15,7 +15,7 @@ cp /home/ubuntu/.ssh/jenkins.* /home/ubuntu/sunny/workspace/$JOB_NAME/keys &&\
 cp /home/ubuntu/sunny/workspace/$JOB_NAME/images/master/src/init.sh /home/ubuntu/sunny/workspace/$JOB_NAME/sample &&\
 
 #docker build --no-cache -t jenkins-master -f /home/ubuntu/sunny/workspace/$JOB_NAME/images/master/Dockerfile ~/coda/.;
-sudo docker build --no-cache -t jenkins-master -f /home/ubuntu/sunny/workspace/$JOB_NAME/images/master/Dockerfile /home/ubuntu/sunny/workspace/$JOB_NAME/.;
+sudo docker build --no-cache -t jenkins-master -f /home/ubuntu/sunny/workspace/$JOB_NAME/images/master/Dockerfile .;
 if [[ "${?}" != 0 ]]; then
     exit_code=1;
 fi;
