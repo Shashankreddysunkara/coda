873600f489c14275a21c65dfc76422e25e34a1ee
Shashank Reddy Sunkara <inusasunny@gmail.com>
2021-02-26 17:18:45 +0530

    Update image-build.Unix.sh

--- images/master/bin/image-build.Unix.sh
+++ images/master/bin/image-build.Unix.sh
@@ -2,8 +2,8 @@
 
 exit_code=0;
 
-mkdir -p keys;
-mkdir -p sample;
+mkdir -p /home/ubuntu/keys;
+mkdir -p /home/ubuntu/sample;
 mkdir -p /home/ubuntu/sunny/sunny_jenkins_master_container_home_volume;
 mkdir -p /home/ubuntu/sunny/sunny_jenkins_log_volume;
 
@@ -11,8 +11,8 @@ mkdir -p /home/ubuntu/sunny/sunny_jenkins_log_volume;
 #    cp ~/coda/images/master/src/init.sh ./sample &&\
 #    docker build --no-cache -t jenkins-master -f ~/coda/images/master/Dockerfile ~/coda/.;
 
-cp /home/ubuntu/.ssh/jenkins.* ./keys &&\
-cp /home/ubuntu/sunny/workspace/$JOB_NAME/images/master/src/init.sh ./sample &&\
+cp /home/ubuntu/.ssh/jenkins.* ./home/ubuntu/keys &&\
+cp /home/ubuntu/sunny/workspace/$JOB_NAME/images/master/src/init.sh ./home/ubuntu/sample &&\
 
 #docker build --no-cache -t jenkins-master -f /home/ubuntu/sunny/workspace/$JOB_NAME/images/master/Dockerfile ~/coda/.;
 sudo docker build --no-cache -t jenkins-master -f /home/ubuntu/sunny/workspace/$JOB_NAME/images/master/Dockerfile /home/ubuntu/sunny/workspace/$JOB_NAME/.;
