bb7163a5c5e78555be7fb740121cb6fa326c5382
Shashank Reddy Sunkara <inusasunny@gmail.com>
2021-03-03 16:45:15 +0530

    Update container-run.Unix.sh
    jenkins master container to 8081

--- images/master/bin/container-run.Unix.sh
+++ images/master/bin/container-run.Unix.sh
@@ -28,7 +28,7 @@ exit_code=0;
 sudo docker run \
     -d \
     --name jenkins-master \
-    -p 8080:8080 \
+    -p 8081:8081 \
     -p 50000:50000 \
     -v $(which docker):/usr/bin/docker \
     -v /var/run/docker.sock:/var/run/docker.sock \
